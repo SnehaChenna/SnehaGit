efcf35ca22d0cdb555fd0c4ec25e7641171d5c4a
Sneha <sneha.chenna08@gmail.com>
2024-03-25 14:28:51 +0100

    Task1 created and Test1 updated

--- src/Test1.java
+++ src/Test1.java
@@ -2,5 +2,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Git practice homework");
         System.out.println("JAVA Programming");
+        System.out.println("??????????????");
     }
 }
